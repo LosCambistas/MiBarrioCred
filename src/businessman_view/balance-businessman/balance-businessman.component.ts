7c1ef2df6c42a5dc9589f15a2c6d8b2fe4934660
Tzabito <miguel_anxel@hotmail.com>
2024-06-10 18:54:49 -0500

    feat: add businessman-balance

---
+++ src/businessman_view/balance-businessman/balance-businessman.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-businessman',
+  templateUrl: './balance-businessman.component.html',
+  styleUrl: './balance-businessman.component.css'
+})
+export class BalanceBusinessmanComponent {
+
+}
