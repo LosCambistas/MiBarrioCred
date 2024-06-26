dec1ed8fe7b7d7eae2567ee3ef14bef0c8eb5f9f
Tzabito <miguel_anxel@hotmail.com>
2024-06-26 00:24:48 -0500

    feature: update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {RegisterComponent} from "../screens/register/register.component";
-import {MenuClientComponent} from "../client_view/menu-client/menu-client.component";
-import {BalanceClientComponent} from "../client_view/balance-client/balance-client.component";
+import {RegisterComponent} from "./screens/register/register.component";
+import {MenuClientComponent} from "./client_view/menu-client/menu-client.component";
+import {BalanceClientComponent} from "./client_view/balance-client/balance-client.component";
 import {MatIcon} from '@angular/material/icon';
 import {NgOptimizedImage} from "@angular/common";
-import {Balance2ClientComponent} from "../client_view/balance2-client/balance2-client.component";
-import {TransactionClientComponent} from "../client_view/transaction-client/transaction-client.component";
-import {PaymentClientComponent} from "../client_view/payment-client/payment-client.component";
+import {Balance2ClientComponent} from "./client_view/balance2-client/balance2-client.component";
+import {TransactionClientComponent} from "./client_view/transaction-client/transaction-client.component";
+import {PaymentClientComponent} from "./client_view/payment-client/payment-client.component";
 import {
   RegisterPaymentsClientComponent
-} from "../client_view/register-payments-client/register-payments-client.component";
-import {MenuComponent} from "../screens/menu/menu.component";
-import {LoginComponent} from "../screens/login/login.component";
-import {MenuBusinessmanComponent} from "../businessman_view/menu-businessman/menu-businessman.component";
-import {BalanceBusinessmanComponent} from "../businessman_view/balance-businessman/balance-businessman.component"
-import {Balance2BusinessmanComponent} from "../businessman_view/balance2-businessman/balance2-businessman.component";
+} from "./client_view/register-payments-client/register-payments-client.component";
+import {MenuComponent} from "./screens/menu/menu.component";
+import {LoginComponent} from "./screens/login/login.component";
+import {MenuBusinessmanComponent} from "./businessman_view/menu-businessman/menu-businessman.component";
+import {BalanceBusinessmanComponent} from "./businessman_view/balance-businessman/balance-businessman.component"
+import {Balance2BusinessmanComponent} from "./businessman_view/balance2-businessman/balance2-businessman.component";
 import {
   RegisterPaymentsBusinessmanComponent
-} from "../businessman_view/register-payments-businessman/register-payments-businessman.component";
-import {ReportsBusinessmanComponent} from "../businessman_view/reports-businessman/reports-businessman.component";
+} from "./businessman_view/register-payments-businessman/register-payments-businessman.component";
+import {ReportsBusinessmanComponent} from "./businessman_view/reports-businessman/reports-businessman.component";
 
 @NgModule({
   declarations: [
@@ -41,14 +41,16 @@ import {ReportsBusinessmanComponent} from "../businessman_view/reports-businessm
     BalanceBusinessmanComponent,
     Balance2BusinessmanComponent,
     RegisterPaymentsBusinessmanComponent,
-    ReportsBusinessmanComponent
+    ReportsBusinessmanComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        MatIcon,
-        NgOptimizedImage
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIcon,
+    NgOptimizedImage,
+    ReactiveFormsModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/auth/usuario.ts
@@ -0,0 +1,18 @@
+// usuarios/usuario.model.ts
+export class Usuario {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    userType: string;
+    balance: number;
+
+    constructor(firstName: string, lastName: string, email: string, password: string, userType: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+        this.balance = 0;
+    }
+}
