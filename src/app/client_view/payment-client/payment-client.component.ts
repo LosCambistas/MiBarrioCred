dc835a7c9e40674c1abc23c9c00955679aa2e97c
Tzabito <miguel_anxel@hotmail.com>
2024-06-28 06:27:13 -0500

    feature: update

--- src/app/client_view/payment-client/payment-client.component.ts
+++ src/app/client_view/payment-client/payment-client.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PaymentService} from "../../auth/payment.service";
+import {map, Observable, of} from "rxjs";
+
+interface Payment {
+  store: string;
+  amount: number;
+  date: Date;
+}
 
 @Component({
   selector: 'app-payment-client',
   templateUrl: './payment-client.component.html',
   styleUrl: './payment-client.component.css'
 })
-export class PaymentClientComponent {
 
+export class PaymentClientComponent implements OnInit {
+  stores: string[] = [];
+  selectedStore: string = '';
+  futurePayments$: Observable<Payment[]> = of([]); // Observable para los pagos futuros
+
+  constructor(private paymentService: PaymentService) {}
+
+  ngOnInit() {
+    // Suscribirse al observable payments$ para obtener los pagos
+    this.paymentService.payments$.subscribe(payments => {
+      this.stores = Object.keys(payments);
+      if (this.selectedStore) {
+        this.futurePayments$ = this.paymentService.payments$.pipe(
+            map(payments => payments[this.selectedStore] || [])
+        );
+      }
+    });
+  }
+
+  onStoreChange(event: Event) {
+    const store = (event.target as HTMLSelectElement).value;
+    this.selectedStore = store;
+    this.futurePayments$ = this.paymentService.payments$.pipe(
+        map(payments => payments[store] || [])
+    );
+  }
 }
--- src/app/client_view/transaction-client/transaction-client.component.ts
+++ src/app/client_view/transaction-client/transaction-client.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TransactionService} from "../../auth/transaction.service";
+import {PaymentService} from "../../auth/payment.service";
+
+interface Transaction {
+  nombreTienda: string;
+  saldo: number;
+  fecha: string;
+}
 
 @Component({
   selector: 'app-transaction-client',
   templateUrl: './transaction-client.component.html',
   styleUrl: './transaction-client.component.css'
 })
-export class TransactionClientComponent {
+export class TransactionClientComponent implements OnInit{
+  transactions: Transaction[] = [];
 
+  constructor(private paymentService: PaymentService) { }
+
+  ngOnInit(): void {
+    this.paymentService.getTransactions().subscribe(transactions => {
+      this.transactions = transactions;
+    });
+  }
 }
