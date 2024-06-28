dfc2cac6f9b1cddad2aa1526f636085b64eb73cb
CamilaLoli <u202110385@upc.edu.pe>
2024-06-28 02:55:05 -0500

    feat: added reports.

--- src/app/businessman_view/reports-businessman/reports-businessman.component.ts
+++ src/app/businessman_view/reports-businessman/reports-businessman.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-reports-businessman',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './reports-businessman.component.css'
 })
 export class ReportsBusinessmanComponent {
+  @Input() reports: any[] = [];
+
+  constructor() {
+  }
+
 
 }
